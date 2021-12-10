9a741c117c046126a0dac38a1a9e6ecef65396d7
Vlad Paraschiv <vladparaschiv03@gmail.com>
2021-12-10 13:46:50 +0200

    update game objects

--- Version_1/src/main/java/PIUGame/GameObjects/UIManager.java
+++ Version_1/src/main/java/PIUGame/GameObjects/UIManager.java
@@ -1,66 +1,50 @@
 package PIUGame.GameObjects;
+
 import PIUGame.RefLinks;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.awt.*;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
-
+@Getter
+@Setter
 public class UIManager {
 
     private RefLinks refLink;
     private ArrayList<UIObject> objects;
 
-    public UIManager(RefLinks refLink){
+    public UIManager(RefLinks refLink) {
         this.refLink = refLink;
         objects = new ArrayList<UIObject>();
     }
 
-    public void Update(){
-        for(UIObject o : objects){
+    public void Update() {
+        for (UIObject o : objects) {
             o.Update();
         }
     }
 
-    public void Draw(Graphics g){
-        for(UIObject o : objects){
+    public void Draw(Graphics g) {
+        for (UIObject o : objects) {
             o.Draw(g);
         }
     }
 
-    public void onMouseMove(MouseEvent e){
-        for(UIObject o : objects){
+    public void onMouseMove(MouseEvent e) {
+        for (UIObject o : objects) {
             o.onMouseMove(e);
         }
     }
 
-    public void onMouseReleased(MouseEvent e){
-        for(UIObject o : objects){
+    public void onMouseReleased(MouseEvent e) {
+        for (UIObject o : objects) {
             o.onMouseRelease(e);
         }
     }
 
-    public void addObject(UIObject o){
+    public void addObject(UIObject o) {
         objects.add(o);
-    }
-
-    public void removeObject(UIObject o){
-        objects.remove(o);
-    }
-
-    public RefLinks getRefLink() {
-        return refLink;
-    }
-
-    public void setRefLink(RefLinks refLink) {
-        this.refLink = refLink;
-    }
-
-    public ArrayList<UIObject> getObjects() {
-        return objects;
-    }
-
-    public void setObjects(ArrayList<UIObject> objects) {
-        this.objects = objects;
     }
 }
--- Version_1/src/main/java/PIUGame/GameObjects/UIObject.java
+++ Version_1/src/main/java/PIUGame/GameObjects/UIObject.java
@@ -1,8 +1,13 @@
 package PIUGame.GameObjects;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.awt.*;
 import java.awt.event.MouseEvent;
 
+@Getter
+@Setter
 public abstract class UIObject {
 
     protected float x, y;
@@ -10,70 +15,27 @@ public abstract class UIObject {
     protected Rectangle bounds;
     protected boolean hovering = false;
 
-    public UIObject(float x, float y, int width, int height){
+    public UIObject(float x, float y, int width, int height) {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        bounds = new Rectangle((int)x, (int)y, width, height);
+        bounds = new Rectangle((int) x, (int) y, width, height);
     }
 
-    public abstract void Update();
-    public abstract void Draw(Graphics g);
-    public abstract void onClick();
+    public void Update() {};
 
-    public void onMouseMove(MouseEvent e){
-        if(bounds.contains(e.getX(), e.getY())){
-            hovering = true;
-        }else{
-            hovering = false;
-        }
+    public void Draw(Graphics g) {};
+
+    public void onClick() {};
+
+    public void onMouseMove(MouseEvent e) {
+        hovering = bounds.contains(e.getX(), e.getY());
     }
 
-    public void onMouseRelease(MouseEvent e){
-        if(hovering){
+    public void onMouseRelease(MouseEvent e) {
+        if (hovering) {
             onClick();
         }
-    }
-
-
-    public float getX() {
-        return x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public int getWidth() {
-        return width;
-    }
-
-    public int getHeight() {
-        return height;
-    }
-
-    public boolean isHovering() {
-        return hovering;
-    }
-
-    public void setX(float x) {
-        this.x = x;
-    }
-
-    public void setY(float y) {
-        this.y = y;
-    }
-
-    public void setWidth(int width) {
-        this.width = width;
-    }
-
-    public void setHeight(int height) {
-        this.height = height;
-    }
-
-    public void setHovering(boolean hovering) {
-        this.hovering = hovering;
     }
 }
